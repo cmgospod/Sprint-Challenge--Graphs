e3d49a51d9498ef7ffff99e4b05b838dbf1c415e
cmgospod <cmgospod@users.noreply.github.com>
2020-03-20 13:05:18 -0500

    tests passed, 218920 moves

--- adv.py
+++ adv.py
@@ -1,6 +1,7 @@
 from room import Room
 from player import Player
 from world import World
+from util import Queue
 
 import random
 from ast import literal_eval
@@ -24,10 +25,85 @@ world.load_graph(room_graph)
 world.print_rooms()
 
 player = Player(world.starting_room)
-
-# Fill this out with directions to walk
-# traversal_path = ['n', 'n']
 traversal_path = []
+# Fill this out with directions to walk
+opposites_dict = {'n': 's', 'e': 'w', 's': 'n', 'w': 'e'}
+traversal_graph = {player.current_room.id: {}}
+exits = player.current_room.get_exits()
+for element in exits:
+    if element not in traversal_graph[player.current_room.id].keys():
+        traversal_graph[player.current_room.id][element] = '?'
+
+while len(traversal_graph) < 500:
+    while True:
+        exits = player.current_room.get_exits()
+        new_exits = [element for element in exits if traversal_graph[player.current_room.id][element] == '?']
+        print(f' new exits:{new_exits}')
+        print(player.current_room.id)
+        if len(new_exits) == 0:
+            break
+        direction_of_travel = random.choice(new_exits)
+        last_room = player.current_room.id
+        player.travel(direction_of_travel)
+        traversal_path.append(direction_of_travel)
+        traversal_graph[last_room][direction_of_travel] = player.current_room.id
+        if traversal_graph.get(player.current_room.id) == None:
+            traversal_graph[player.current_room.id] = {}
+        else:
+            pass
+        exits = player.current_room.get_exits()
+        for element in exits:
+            if element not in traversal_graph[player.current_room.id].keys():
+                traversal_graph[player.current_room.id][element] = '?'
+        traversal_graph[player.current_room.id][opposites_dict[direction_of_travel]] = last_room
+
+    q = Queue()
+    q.enqueue([player.current_room.id])
+    visited = set()
+    while q.size() > 0:
+        path = q.dequeue()
+        v = path[-1]
+        if v not in visited:
+            visited.add(v)
+            print(type(v))
+            if '?' in traversal_graph[v].values():
+                break
+            for neighbor in traversal_graph[v].values():
+                path_copy = path.copy()
+                path_copy.append(neighbor)
+                q.enqueue(path_copy)
+    directed_path = []
+    print(path)
+    for element in path[1:]:
+        print(traversal_graph[player.current_room.id])
+        for key, value in traversal_graph[player.current_room.id].items():
+            print(element)
+            print(value)
+            if element == value:
+                direction_to_go = key
+                break
+        player.travel(direction_to_go)
+        traversal_path.append(direction_to_go)
+
+        # key_list = list(traversal_graph[player.current_room.id].keys())
+        # value_list = list(traversal_graph[player.current_room.id].values())
+        # traversal_path.append(key_list[value_list.index(element)])
+        # player.travel(key_list[value_list.index(element)])
+
+
+
+    backtrack = traversal_path[::-1]
+    for path in backtrack:
+        player.travel(opposites_dict[path])
+        traversal_path.append(opposites_dict[path])
+        if '?' in traversal_graph[player.current_room.id].values():
+            print('yup')
+            break
+        else:
+            print('nope')
+            pass
+
+
 
 
 
